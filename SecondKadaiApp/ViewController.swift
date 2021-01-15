5d79b862721e8960c2ae72787fd3e8ad2e92321c
takeshi.ootsuka <takeshi.otsuka111@gmail.com>
2021-01-15 15:12:38 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by user on 2021/01/15.
+//  Copyright © 2021 takeshi-2983. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
